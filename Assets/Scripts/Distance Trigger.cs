abdef3be9cf7f7ccbd4f0aee4543ff2ab1fcb3e7
Logan Wiley <lwmh853@gmail.com>
2024-02-29 14:01:48 -0600

    stuff
    stuff 2

--- Assets/Scripts/Distance Trigger.cs
+++ Assets/Scripts/Distance Trigger.cs
@@ -1,46 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class DistanceTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
     private Transform target;
-
     [SerializeField]
     private float activationDistance = 3.0f;
     [SerializeField]
     private float resetDelay = 10.0f;
     [SerializeField]
-    private string triggerName = "StartAnim";
-    private float timer;
+    private string triggerName = "NextAnim";
+    [SerializeField]
+    private AudioSource audioSource; // Reference to the AudioSource component
 
+    private float timer;
     private Animator anim;
-    private AudioSource aud;
 
     void Start()
     {
-        anim= this.GetComponent<Animator>();
-        aud= this.GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(this.transform.position, target.position);
-        if (distance < activationDistance && timer <=0)
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance < activationDistance && timer <= 0)
         {
             Activate();
         }
-        if (timer > 0) timer += Time.deltaTime;
+
+        if (timer > 0) timer -= Time.deltaTime;
     }
+
     void Activate()
     {
+        // Play animation
         anim.SetTrigger(triggerName);
-        aud.Play();
-        timer  = resetDelay;
+
+        // Play sound
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+
+        timer = resetDelay;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, activationDistance);
     }
 }
